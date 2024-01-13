589daedfebec866c24b6bb8e8235e80d8d093ecb
yoshi <38038621+yoshi07re@users.noreply.github.com>
2024-01-13 16:33:01 +0900

    Refactor component import and add initial props

--- plopfile.mjs
+++ plopfile.mjs
@@ -3,14 +3,33 @@ export default function (plop) {
     description: 'create a new component',
     prompts: [
       {
+        type: 'list',
+        name: 'pathType',
+        message: 'Do you want to select the path or input it?',
+        choices: ['Select', 'Input'],
+      },
+      {
+        type: 'list',
+        name: 'path',
+        choices: ['app/pages', 'app/_components', 'app/_layout'],
+        message: 'Where is the path to create components? (e.g. components)',
+        when: (answers) => answers.pathType === 'Select',
+      },
+      {
         type: 'input',
         name: 'path',
         message: 'Where is the path to create components? (e.g. components)',
+        when: (answers) => answers.pathType === 'Input',
       },
       {
         type: 'input',
         name: 'name',
         message: 'What is the component name? (e.g. button)',
+      },
+      {
+        type: 'input',
+        name: 'defaultElement',
+        message: 'What is the Element name? (e.g. div, span, button etc.)',
       },
     ],
     actions: [
