2afed87d6c77ba343cec37e0082c266d6fc9a92b
yoshi <38038621+yoshi07re@users.noreply.github.com>
2024-01-14 15:28:59 +0900

    Refactor page transition component to handle scroll restoration

--- app/_layout/PageTransition/index.tsx
+++ app/_layout/PageTransition/index.tsx
@@ -1,3 +1,68 @@
+// 'use client';
+
+// import { AnimatePresence, motion } from 'framer-motion';
+// import { useSelectedLayoutSegment } from 'next/navigation';
+// import {
+//   ElementRef,
+//   ReactNode,
+//   forwardRef,
+//   useContext,
+//   useEffect,
+// } from 'react';
+
+// import { LenisContext } from '@/app/_layout/LenisLayout';
+// import { FrozenRouter } from '@/app/_layout/PageTransition/frozenRouter';
+
+// interface ChildProps {
+//   children: ReactNode;
+// }
+
+// const Child = forwardRef<ElementRef<typeof motion.div>, ChildProps>(
+//   (props, ref) => {
+//     return (
+//       <motion.div
+//         ref={ref}
+//         initial={{ opacity: 0, y: 40 }}
+//         animate={{ opacity: 1, y: 0 }}
+//         exit={{ opacity: 0, y: -40 }}
+//         transition={{ duration: 0.5, delay: 0.1 }}
+//       >
+//         <FrozenRouter>{props.children}</FrozenRouter>
+//       </motion.div>
+//     );
+//   },
+// );
+
+// Child.displayName = 'Child';
+
+// interface PageTransitionProps {
+//   children: ReactNode;
+// }
+
+// export const PageTransition = (props: PageTransitionProps) => {
+//   const segment = useSelectedLayoutSegment();
+//   const lenis = useContext(LenisContext);
+
+//   useEffect(() => {
+//     if ('scrollRestoration' in history) {
+//       history.scrollRestoration = 'manual';
+//     }
+//   }, []);
+
+//   return (
+//     <>
+//       <AnimatePresence
+//         mode="wait"
+//         onExitComplete={() => {
+//           lenis?.scrollTo(0, { immediate: true });
+//         }}
+//       >
+//         <Child key={segment}>{props.children}</Child>
+//       </AnimatePresence>
+//     </>
+//   );
+// };
+
 'use client';
 
 import { AnimatePresence, motion } from 'framer-motion';
@@ -8,6 +73,7 @@ import {
   forwardRef,
   useContext,
   useEffect,
+  useState,
 } from 'react';
 
 import { LenisContext } from '@/app/_layout/LenisLayout';
@@ -42,11 +108,23 @@ interface PageTransitionProps {
 export const PageTransition = (props: PageTransitionProps) => {
   const segment = useSelectedLayoutSegment();
   const lenis = useContext(LenisContext);
+  const [beforeScrollTop, setBeforeScrollTop] = useState(0);
+  const [isBackOrForward, setIsBackOrForward] = useState(false);
 
   useEffect(() => {
     if ('scrollRestoration' in history) {
       history.scrollRestoration = 'manual';
     }
+
+    const handlePopState = () => {
+      setIsBackOrForward(true);
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
   }, []);
 
   return (
@@ -54,7 +132,18 @@ export const PageTransition = (props: PageTransitionProps) => {
       <AnimatePresence
         mode="wait"
         onExitComplete={() => {
-          lenis?.scrollTo(0, { immediate: true });
+          if (isBackOrForward) {
+            lenis?.scrollTo(beforeScrollTop, { immediate: true });
+          } else {
+            const currentScrollY = window.scrollY;
+            setBeforeScrollTop(currentScrollY);
+            lenis?.scrollTo(0, { immediate: true });
+            // console.log(
+            //   'Saving scroll position on page transition:',
+            //   currentScrollY,
+            // );
+          }
+          setIsBackOrForward(false);
         }}
       >
         <Child key={segment}>{props.children}</Child>
