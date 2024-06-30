d9155216f2733f9a9cc54d0d18283cbe7a81494e
yoshi <38038621+yoshi07re@users.noreply.github.com>
2024-06-30 12:39:26 +0900

    テスト

--- app/_layout/PageTransition/index.tsx
+++ app/_layout/PageTransition/index.tsx
@@ -23,10 +23,10 @@
 //     return (
 //       <motion.div
 //         ref={ref}
-//         initial={{ opacity: 0, y: 40 }}
+//         initial={{ opacity: 0, y: '40svh' }}
 //         animate={{ opacity: 1, y: 0 }}
-//         exit={{ opacity: 0, y: -40 }}
-//         transition={{ duration: 0.5, delay: 0.1 }}
+//         exit={{ opacity: 0, y: '-40svh' }}
+//         transition={{ duration: 0.5 }}
 //       >
 //         <FrozenRouter>{props.children}</FrozenRouter>
 //       </motion.div>
@@ -87,210 +87,60 @@
 //   );
 // };
 
-// 'use client';
-
-// import { AnimatePresence, motion } from 'framer-motion';
-// import { usePathname } from 'next/navigation';
-// import {
-//   ElementRef,
-//   ReactNode,
-//   forwardRef,
-//   useContext,
-//   useEffect,
-//   useState,
-// } from 'react';
-
-// import { LenisContext } from '@/app/_layout/LenisLayout';
-// import { FrozenRouter } from '@/app/_layout/PageTransition/frozenRouter';
-
-// interface ChildProps {
-//   children: ReactNode;
-//   isBackOrForward: boolean;
-// }
-
-// const Child = forwardRef<ElementRef<typeof motion.div>, ChildProps>(
-//   ({ children, isBackOrForward }, ref) => {
-//     const initial = isBackOrForward ? { opacity: 0 } : { opacity: 0, y: 40 };
-//     const animate = isBackOrForward ? { opacity: 1 } : { opacity: 1, y: 0 };
-//     const exit = isBackOrForward ? { opacity: 0 } : { opacity: 0, y: -40 };
-
-//     return (
-//       <motion.div
-//         ref={ref}
-//         initial={initial}
-//         animate={animate}
-//         exit={exit}
-//         transition={{ duration: 0.5, delay: 0.1 }}
-//       >
-//         <FrozenRouter>{children}</FrozenRouter>
-//       </motion.div>
-//     );
-//   },
-// );
-
-// Child.displayName = 'Child';
-
-// interface PageTransitionProps {
-//   children: ReactNode;
-// }
-
-// export const PageTransition = (props: PageTransitionProps) => {
-//   const segment = usePathname();
-//   const lenis = useContext(LenisContext);
-//   const [beforeScrollTop, setBeforeScrollTop] = useState(0);
-//   const [isBackOrForward, setIsBackOrForward] = useState(false);
-
-//   useEffect(() => {
-//     if ('scrollRestoration' in history) {
-//       history.scrollRestoration = 'manual';
-//     }
-
-//     const handlePopState = () => {
-//       setIsBackOrForward(true);
-//     };
-
-//     window.addEventListener('popstate', handlePopState);
-
-//     return () => {
-//       window.removeEventListener('popstate', handlePopState);
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <AnimatePresence
-//         mode="wait"
-//         onExitComplete={() => {
-//           if (isBackOrForward) {
-//             const currentScrollY = window.scrollY;
-//             lenis?.scrollTo(beforeScrollTop, { immediate: true });
-//             setBeforeScrollTop(currentScrollY);
-//             console.log('ブラウザの戻る/進む', currentScrollY);
-//           } else {
-//             const currentScrollY = window.scrollY;
-//             setBeforeScrollTop(currentScrollY);
-//             lenis?.scrollTo(0, { immediate: true });
-//             console.log('通常のページ遷移', currentScrollY);
-//           }
-//           setIsBackOrForward(false);
-//         }}
-//       >
-//         <Child key={segment} isBackOrForward={isBackOrForward}>
-//           {props.children}
-//         </Child>
-//       </AnimatePresence>
-//     </>
-//   );
-// };
-
 'use client';
 
 import { AnimatePresence, motion } from 'framer-motion';
+import { LayoutRouterContext } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { usePathname } from 'next/navigation';
 import {
   ElementRef,
   ReactNode,
   forwardRef,
   useContext,
-  useEffect,
+  useLayoutEffect,
+  useRef,
   useState,
 } from 'react';
 
-import { LenisContext } from '@/app/_layout/LenisLayout';
-import { FrozenRouter } from '@/app/_layout/PageTransition/frozenRouter';
-
 interface ChildProps {
   children: ReactNode;
-  isBackOrForward: boolean;
 }
 
-const perspective = {
-  initial: {
-    scale: 1,
-    y: 0,
-  },
-  enter: {
-    scale: 1,
-    y: 0,
-  },
-  exit: {
-    scale: 0.9,
-    y: -150,
-    opacity: 0.5,
-    transition: {
-      duration: 1.2,
-      ease: [0.76, 0, 0.24, 1],
-    },
-  },
-};
+const usePreviousValue = <T,>(value: T): T | undefined => {
+  const prevValue = useRef<T>();
 
-const slide = {
-  initial: {
-    y: '100vh',
-  },
-  animate: {
-    y: '100vh',
-  },
-  exit: {
-    y: 0,
-    transition: {
-      duration: 1,
-      ease: [0.76, 0, 0.24, 1],
-    },
-  },
-};
+  useLayoutEffect(() => {
+    prevValue.current = value;
+  }, [value]);
 
-const opacity = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: {
-    opacity: 1,
-  },
+  return prevValue.current;
 };
 
 const Child = forwardRef<ElementRef<typeof motion.div>, ChildProps>(
-  ({ children, isBackOrForward }, ref) => {
-    const animation = isBackOrForward
-      ? {
-          initial: { opacity: 0 },
-          animate: { opacity: 1 },
-          exit: { opacity: 0 },
-          transition: { duration: 0.5, delay: 0.1 },
-        }
-      : {};
+  ({ children }, ref) => {
+    const context = useContext(LayoutRouterContext ?? {});
+    const prevContext = usePreviousValue(context) || null;
 
-    return isBackOrForward ? (
-      <motion.div ref={ref} {...animation}>
-        <FrozenRouter>{children}</FrozenRouter>
-      </motion.div>
-    ) : (
+    const segment = usePathname();
+    const prevSegment = usePreviousValue(segment);
+
+    const changed =
+      segment !== prevSegment &&
+      segment !== undefined &&
+      prevSegment !== undefined;
+
+    return (
       <>
         <motion.div
-          className="fixed inset-0 z-10 h-screen w-full"
-          initial={slide.initial}
-          animate={slide.animate}
-          exit={slide.exit}
-        />
-        <motion.div
-          initial={perspective.initial}
-          animate={perspective.enter}
-          exit={perspective.exit}
+          ref={ref}
+          initial={{ opacity: 0, y: '40svh' }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: '-40svh' }}
+          transition={{ duration: 0.5 }}
         >
-          <motion.div
-            className="absolute inset-0"
-            initial={opacity.initial}
-            animate={opacity.animate}
-            exit={opacity.exit}
-          >
-            <FrozenRouter>{children}</FrozenRouter>
-          </motion.div>
+          <LayoutRouterContext.Provider value={changed ? prevContext : context}>
+            {children}
+          </LayoutRouterContext.Provider>
         </motion.div>
       </>
     );
@@ -303,50 +153,41 @@ interface PageTransitionProps {
   children: ReactNode;
 }
 
-export const PageTransition = (props: PageTransitionProps) => {
+export const PageTransition = ({ children }: PageTransitionProps) => {
   const segment = usePathname();
-  const lenis = useContext(LenisContext);
   const [beforeScrollTop, setBeforeScrollTop] = useState(0);
   const [isBackOrForward, setIsBackOrForward] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if ('scrollRestoration' in history) {
       history.scrollRestoration = 'manual';
     }
 
-    const handlePopState = () => {
-      setIsBackOrForward(true);
-    };
+    const handlePopState = () => setIsBackOrForward(true);
 
     window.addEventListener('popstate', handlePopState);
-
-    return () => {
-      window.removeEventListener('popstate', handlePopState);
-    };
+    return () => window.removeEventListener('popstate', handlePopState);
   }, []);
+
+  useLayoutEffect(() => {
+    setBeforeScrollTop(window.scrollY);
+    // console.log('現在のスクロール位置をコンソールに出力:', window.scrollY);
+  }, [segment]);
+
+  const handleExitComplete = () => {
+    // console.log('遷移前のスクロール位置をコンソールに出力:', beforeScrollTop);
+    window.scrollTo({
+      top: isBackOrForward ? beforeScrollTop : 0,
+      behavior: 'auto',
+    });
+    setIsBackOrForward(false);
+    // console.log('scrollTo呼び出し後のスクロール位置をコンソールに出力:', window.scrollY);
+  };
 
   return (
     <>
-      <AnimatePresence
-        mode="wait"
-        onExitComplete={() => {
-          if (isBackOrForward) {
-            const currentScrollY = window.scrollY;
-            lenis?.scrollTo(beforeScrollTop, { immediate: true });
-            setBeforeScrollTop(currentScrollY);
-            console.log('ブラウザの戻る/進む', currentScrollY);
-          } else {
-            const currentScrollY = window.scrollY;
-            setBeforeScrollTop(currentScrollY);
-            lenis?.scrollTo(0, { immediate: true });
-            console.log('通常のページ遷移', currentScrollY);
-          }
-          setIsBackOrForward(false);
-        }}
-      >
-        <Child key={segment} isBackOrForward={isBackOrForward}>
-          {props.children}
-        </Child>
+      <AnimatePresence mode="wait" onExitComplete={handleExitComplete}>
+        <Child key={segment}>{children}</Child>
       </AnimatePresence>
     </>
   );
