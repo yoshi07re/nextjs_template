93c0465a917f00fdb6d71a9be048de548603ed7a
yoshi <38038621+yoshi07re@users.noreply.github.com>
2024-07-16 07:36:17 +0900

    add

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+      exclude: [/app\/icon\.svg$/],
+    });
+    return config;
+  },
+  // images: {
+  //   disableStaticImages: true, // importした画像の型定義設定を無効にする
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'images.microcms-assets.io',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
